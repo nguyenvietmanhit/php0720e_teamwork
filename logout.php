76a1fa2d04f2cbd2328dc6bc6362682617386ac9
Manh <nvmanh@cmc.com.vn>
2020-11-24 20:28:29 +0700

    a

--- logout.php
+++ logout.php
@@ -6,3 +6,8 @@
  * Time: 8:22 PM
  */
 echo "nvmanh logout";
+// Checkout về nhánh master
+// git pull
+// tạo branch mới và checkout về branch mới
+// Sau khi tạo nhánh mới, chỉnh sửa file logout.php
+// Git add, git commit, git push origin <branch-hiện-tại>
